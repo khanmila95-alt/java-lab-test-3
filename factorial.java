2991ce711ccf92e95973e53e2b09fa6f5ad4d4d4
khanmila95-alt <khanmila95@gmail.com>
2026-01-22 21:06:13 +0600

    Implement FactorionUtil with factorial and isFactorion methods

---
+++ factorial.java
@@ -0,0 +1,25 @@
+public class FactorionUtil {
+
+   
+    public static int factorial(int n) {
+        int fact = 1;
+        for (int i = 1; i <= n; i++) {
+            fact = fact * i;
+        }
+        return fact;
+    }
+
+   
+    public static boolean isFactorion(int number) {
+        int temp = number;
+        int sum = 0;
+
+        while (temp > 0) {
+            int digit = temp % 10;
+            sum += factorial(digit);
+            temp = temp / 10;
+        }
+
+        return sum == number;
+    }
+}
